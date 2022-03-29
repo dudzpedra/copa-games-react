272d37f41f3e63accf05e2a9251a97f7275c0032
Eduardo <eduardo@sanp.com.br>
2022-03-29 02:47:16 -0300

    feature: added style for card selected

--- src/components/GameCard/index.jsx
+++ src/components/GameCard/index.jsx
@@ -1,8 +1,11 @@
+import { useSelector } from 'react-redux';
 import styles from './GameCard.module.css'
 
 const GameCard = ({ item }) => {
+  const selectedGames = useSelector(state => state.games.selectedGames)
+  const isSelected = selectedGames.find(game => game.id === item.id)
   return (
-    <div className={styles.card}>
+    <div className={isSelected ? styles.selected : styles.card}>
       <img alt={item.titulo} src={item.urlImagem} />
       <p>{item.titulo}</p>
       <p>{item.ano}</p>
